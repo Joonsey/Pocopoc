84815bd2ca36fb3d351585ec748ec9b012ed9ca1
Johannes <jonseyplayz@gmail.com>
2022-04-05 07:04:58 +0200

    Changed main logic

--- main.py
+++ main.py
@@ -15,29 +15,39 @@ async def on_ready():
     change_status.start()
     print('Logged in as', client.user.name)
     current_guild = client.guilds[0]
-    general_voice_chat = current_guild.voice_channels[0]
+    target_voice_chat = current_guild.voice_channels[0]
     # print(current_guild)
-    # print(general_voice_chat)
+    # print(target_voice_chat)
     # print(current_guild.members)
-    await general_voice_chat.connect()
+
+    await target_voice_chat.connect()
 
 @client.command(aliases=['raidtime'])
 async def raid_time(ctx):
-    members = []
+    """ An officer can call this command to move all active voice raid members to raid chat """
+    if 'officer' in [c.name.lower() for c in ctx.author.roles]: 
+        members = []
 
-    for role in client.guilds[0].roles:
-        if role.name.lower() in ['trial raider', 'raider', 'raider trial'] or role.id in [960743045849038849]:
-            for m in role.members:
-                if m.voice is not None:
-                    members.append(m)
+        """ Adds all members from specific channels to a list"""
+        for channel in client.guilds[0].channels:
+            if channel.name.lower() in ['trial raider', 'raider', 'raider trial'] or channel.id in [960743045849038849]:
+                for m in channel.members:
+                    if m.voice is not None:
+                        members.append(m)
 
-    if ctx.author not in members and ctx.author.voice is not None:
-        members.append(ctx.author)
-    
-    general_voice_chat = client.guilds[0].voice_channels[0]
-    for member in members:
-        print(f"Moving {member} to {general_voice_chat}")
-        await member.move_to(general_voice_chat, reason="Raid time!")
-
+        """ Determines the target voice channel to move all members to """
+        for channel in client.guilds[0].voice_channels:
+            if channel.id == 648591623638220830:
+                target_voice_chat = channel
+                break
+            else:        
+                target_voice_chat = client.guilds[0].voice_channels[0]
+        """ Moving all members to target channel """
+        for member in members:
+            if member not in target_voice_chat.members:
+                print(f"Moving {member} to {target_voice_chat}")
+                await member.move_to(target_voice_chat, reason="Raid time!")
+    else:
+        await ctx.send('Nice try fool.')
 if "__main__" == __name__:
     client.run(TOKEN)
